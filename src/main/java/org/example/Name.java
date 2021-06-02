8d7f113dc24bd8b915e8bc7f02b0d1101558bfb5
chad <chadhiggins6@gmail.com>
2021-06-02 12:26:55 -0400

    hello world test

---
+++ src/main/java/org/example/Name.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Name {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World");
+    }
+}
+//git commands
+// git status - what is being push
+// git add - stage changes
+// git commit -m "add a comment"- comfirm changes
+// git push - push to repository
