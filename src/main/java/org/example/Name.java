4e3c81f6e1939f06b2ca913762be16fc1a834cf5
chad <chadhiggins6@gmail.com>
2021-06-03 12:41:21 -0400

    COP3330 Example 1

--- src/main/java/org/example/Name.java
+++ src/main/java/org/example/Name.java
@@ -1,9 +1,32 @@
 package org.example;
 
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solutions
+ *  Copyright 2021 Chad Higgins
+ */
+
+/*
+Create a program that prompts for your name and prints a greeting using your name.
+
+Example Output
+What is your name? Brian
+Hello, Brian, nice to meet you!
+Constraint
+Keep the input, string concatenation, and output separate.
+ */
+
+
+import java.util.Scanner;
+
 public class Name {
     public static void main(String[] args)
     {
-        System.out.println("Hello World");
+        System.out.println("What is your name?");
+        Scanner name = new Scanner(System.in);
+
+        String Name = name.nextLine();
+
+        System.out.println("Hello, " + Name + ", nice to meet you!");
     }
 }
 //git commands
